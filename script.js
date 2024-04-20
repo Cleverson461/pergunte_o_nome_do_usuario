136060d84183ada74000f0fd5af45d8da9015775
Cleverson Nascimento <cleverson461.com>
2024-04-20 10:01:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+/*
+
+Encontre a solução ao problema:
+
+Pergunte o nome do usuário e escreva a mensagem:
+Olá, [nome do usuário]  
+
+Como resolver problemas na linguagem de programação?
+  como melhorar minha lógica de programação?
+
+  1. Identifique o que já sabe e faça! 
+  2. Quebre o grande problema em problemas menores!
+*/
+
+let seuNome = prompt('Qual é o seu nome?')
+alert(`Olá, ${seuNome}`)
